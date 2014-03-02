d3f330ba8a3978b203e78f1c89687125f863297d
Tony Kelman <tony@kelman.net>
2014-03-02 12:43:19 -0800

    add binary readline

--- contrib/cygwin_build.sh
+++ contrib/cygwin_build.sh
@@ -34,8 +34,8 @@ if [ `arch` = x86_64 ]; then
   echo "LLVM_LLC = $PWD/llvm/bin/llc" >> Make.user
   echo "LDFLAGS = -L$PWD/llvm/lib" >> Make.user
   # This binary version doesn't include libgtest or libgtest_main for some reason
-  x86_64-w64-mingw32-ar -r llvm/lib/libgtest.a
-  x86_64-w64-mingw32-ar -r llvm/lib/libgtest_main.a
+  x86_64-w64-mingw32-ar cr llvm/lib/libgtest.a
+  x86_64-w64-mingw32-ar cr llvm/lib/libgtest_main.a
   
   # Download OpenBlas binary
   wget -O openblas.7z "https://drive.google.com/uc?export=download&id=0B4DmELLTwYmlVWxuTU1QOHozbWM" >> get-deps.log 2>&1
@@ -44,11 +44,18 @@ if [ `arch` = x86_64 ]; then
   echo "USE_SYSTEM_LAPACK = 1" >> Make.user
   echo "LIBBLAS = -L$PWD/lib -lopenblas" >> Make.user
   echo "LIBBLASNAME = libopenblas" >> Make.user
+  echo 'override LIBLAPACK = $(LIBBLAS)' >> Make.user
+  echo 'override LIBLAPACKNAME = $(LIBBLASNAME)' >> Make.user
+  
+  # Download readline binary
+  wget http://download.opensuse.org/repositories/windows:/mingw:/win64/openSUSE_Factory/noarch/mingw64-readline-6.2-3.15.noarch.rpm >> get-deps.log 2>&1
+  bsdtar -xf mingw64-readline-6.2-3.15.noarch.rpm
+  echo "USE_SYSTEM_READLINE = 1" >> Make.user
 else
   echo "XC_HOST = i686-pc-mingw32" > Make.user
   echo "override BUILD_MACHINE = i686-pc-cygwin" >> Make.user
   
-  make -C deps get-llvm get-openblas get-lapack > get-deps.log 2>&1
+  make -C deps get-llvm get-openblas get-lapack get-readline > get-deps.log 2>&1
   # OpenBlas uses HOSTCC to compile getarch, but we might not have Cygwin GCC installed
   if [ -z `which gcc 2>/dev/null` ]; then
     echo 'override HOSTCC = $(CROSS_COMPILE)gcc' >> Make.user
@@ -56,7 +63,7 @@ else
 fi
 
 #make -C deps getall >> get-deps.log 2>&1
-make -C deps get-readline get-uv get-pcre get-double-conversion get-openlibm \
+make -C deps get-uv get-pcre get-double-conversion get-openlibm \
   get-openspecfun get-random get-fftw get-suitesparse get-arpack get-unwind \
   get-osxunwind get-gmp get-mpfr get-zlib get-patchelf get-utf8proc >> get-deps.log 2>&1
 
