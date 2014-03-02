0df49f916122b35bf6d24371f15f08e227cc6400
Tony Kelman <tony@kelman.net>
2014-03-02 06:26:01 -0800

    use d2u on configure scripts

--- contrib/cygwin_build.sh
+++ contrib/cygwin_build.sh
@@ -12,18 +12,8 @@
 
 # stop on error
 set -e
-
-git config --global user.email "appveyor@julialang.org"
-git config --global user.name "Julia AppVeyor"
-echo "* text=auto" >>.gitattributes
-rm .git/index     # Remove the index to force git to
-git reset         # re-scan the working directory
-#git status        # Show files that will be normalized
-git add -u
-git add .gitattributes
-git commit -m "Introduce end-of-line normalization"
-
-#dos2unix contrib/relative_path.sh deps/jldownload
+pwd
+dos2unix contrib/relative_path.sh deps/jldownload */*/configure */*/config.status
 
 if [ `arch` = x86_64 ]; then
   XC_HOST=x86_64-w64-mingw32 make
