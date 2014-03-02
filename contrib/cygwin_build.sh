99b7808fde9f093e0d0b158a8f3ba872045df104
Tony Kelman <tony@kelman.net>
2014-03-02 07:53:15 -0800

    get dependencies first, then run d2u on them

--- contrib/cygwin_build.sh
+++ contrib/cygwin_build.sh
@@ -10,18 +10,15 @@
 #
 # This script is intended to be executed from the base julia directory as contrib/cygwin_build.sh
 
-# stop on error
-#set -e
-
-if [ -n "$APPVEYOR" ]; then
-  git config core.eol
-  git config --global core.eol
-  git config --global core.eol lf
-fi
 dos2unix contrib/relative_path.sh deps/jldownload
 
 if [ `arch` = x86_64 ]; then
-  XC_HOST=x86_64-w64-mingw32 make
+  echo "XC_HOST = x86_64-w64-mingw32" > Make.user
 else
-  XC_HOST=i686-pc-mingw32 make
+  echo "XC_HOST = i686-pc-mingw32" > Make.user
 fi
+
+make -C deps getall > get-deps.log
+dos2unix */*/configure
+dos2unix */*/config.status
+make
