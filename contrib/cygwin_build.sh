207c78038ee336f275b6aa5e55beebe48fe344a3
Tony Kelman <tony@kelman.net>
2014-03-02 05:46:18 -0800

    Make git fix the line endings

--- contrib/cygwin_build.sh
+++ contrib/cygwin_build.sh
@@ -2,6 +2,7 @@
 # Script to cross-compile Julia from Cygwin to MinGW assuming the following packages are installed:
 # - dos2unix
 # - make
+# - wget
 # - mingw64-x86_64-gcc-g++ (for 64 bit)
 # - mingw64-x86_64-gcc-fortran (for 64 bit)
 # - mingw-gcc-g++ (for 32 bit)
@@ -9,7 +10,18 @@
 #
 # This script is intended to be executed from the base julia directory as contrib/cygwin_build.sh
 
-dos2unix contrib/relative_path.sh deps/jldownload
+# stop on error
+set -e
+
+#echo "* text=auto" >>.gitattributes
+rm .git/index     # Remove the index to force git to
+git reset         # re-scan the working directory
+#git status        # Show files that will be normalized
+#git add -u
+#git add .gitattributes
+#git commit -m "Introduce end-of-line normalization"
+
+#dos2unix contrib/relative_path.sh deps/jldownload
 
 if [ `arch` = x86_64 ]; then
   XC_HOST=x86_64-w64-mingw32 make
