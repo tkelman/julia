2ce52477dd9a34b18ee0be17feb605bfc357a37d
Tony Kelman <tony@kelman.net>
2014-03-02 08:08:40 -0800

    quiet with more redirects

--- contrib/cygwin_build.sh
+++ contrib/cygwin_build.sh
@@ -2,7 +2,7 @@
 # Script to cross-compile Julia from Cygwin to MinGW assuming the following packages are installed:
 # - dos2unix
 # - make
-# - wget
+# - curl
 # - mingw64-x86_64-gcc-g++ (for 64 bit)
 # - mingw64-x86_64-gcc-fortran (for 64 bit)
 # - mingw-gcc-g++ (for 32 bit)
@@ -10,7 +10,7 @@
 #
 # This script is intended to be executed from the base julia directory as contrib/cygwin_build.sh
 
-dos2unix contrib/relative_path.sh deps/jldownload
+dos2unix contrib/relative_path.sh deps/jldownload 2>&1
 
 if [ `arch` = x86_64 ]; then
   echo "XC_HOST = x86_64-w64-mingw32" > Make.user
@@ -18,7 +18,6 @@ else
   echo "XC_HOST = i686-pc-mingw32" > Make.user
 fi
 
-make -C deps getall > get-deps.log
-dos2unix */*/configure
-dos2unix */*/config.status
+make -C deps getall > get-deps.log 2>&1
+dos2unix */*/configure 2>&1
 make
