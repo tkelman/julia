988c7689d9e89b5ee3ab15ae49d7690540dd6c62
Tony Kelman <tony@kelman.net>
2014-03-02 16:56:37 -0800

    put everything into usr

--- contrib/cygwin_build.sh
+++ contrib/cygwin_build.sh
@@ -33,15 +33,23 @@ if [ `arch` = x86_64 ]; then
   # Download LLVM binary
   wget https://sourceforge.net/projects/mingw-w64-dgn/files/others/llvm-3.3-w64-bin-x86_64-20130804.7z > get-deps.log 2>&1
   bsdtar -xf llvm-3.3-w64-bin-x86_64-20130804.7z
-  # Copy MinGW libs into llvm/bin folder
-  cp /usr/x86_64-w64-mingw32/sys-root/mingw/bin/*.dll llvm/bin
+  if [ -d usr ]; then
+    for i in bin include lib; do
+      mkdir -p usr/$i
+      mv llvm/$i/* usr/$i
+    done
+  else
+    mv llvm usr
+  fi
   echo "USE_SYSTEM_LLVM = 1" >> Make.user
-  echo "LLVM_CONFIG = $PWD/llvm/bin/llvm-config" >> Make.user
-  echo "LLVM_LLC = $PWD/llvm/bin/llc" >> Make.user
-  echo "LDFLAGS = -L$PWD/llvm/lib" >> Make.user
+  echo "LLVM_CONFIG = $PWD/usr/bin/llvm-config" >> Make.user
+  echo "LLVM_LLC = $PWD/usr/bin/llc" >> Make.user
+  #echo "LDFLAGS = -L$PWD/llvm/lib" >> Make.user
+  #echo "CXXFLAGS = -I$PWD/llvm/include" >> Make.user
+  #echo "CFLAGS = -I$PWD/llvm/include" >> Make.user
   # This binary version doesn't include libgtest or libgtest_main for some reason
-  x86_64-w64-mingw32-ar cr llvm/lib/libgtest.a
-  x86_64-w64-mingw32-ar cr llvm/lib/libgtest_main.a
+  x86_64-w64-mingw32-ar cr usr/lib/libgtest.a
+  x86_64-w64-mingw32-ar cr usr/lib/libgtest_main.a
   
   # Download OpenBlas binary
   #wget -O openblas.7z "https://drive.google.com/uc?export=download&id=0B4DmELLTwYmlVWxuTU1QOHozbWM" >> get-deps.log 2>&1
@@ -61,6 +69,7 @@ if [ `arch` = x86_64 ]; then
   wget ftp://rpmfind.net/linux/fedora/linux/releases/20/Everything/x86_64/os/Packages/m/mingw64-readline-6.2-3.fc20.noarch.rpm >> get-deps.log 2>&1
   bsdtar -xf mingw64-readline-6.2-3.fc20.noarch.rpm
   echo "USE_SYSTEM_READLINE = 1" >> Make.user
+  echo "override READLINE = -lreadline -lhistory" >> Make.user
   
   # Download pcre binary
   wget ftp://rpmfind.net/linux/fedora/linux/releases/18/Everything/x86_64/os/Packages/m/mingw64-pcre-8.31-1.fc18.noarch.rpm >> get-deps.log 2>&1
@@ -86,11 +95,15 @@ if [ `arch` = x86_64 ]; then
   wget ftp://rpmfind.net/linux/fedora/linux/releases/20/Everything/x86_64/os/Packages/m/mingw64-zlib-1.2.8-2.fc20.noarch.rpm >> get-deps.log 2>&1
   bsdtar -xf mingw64-zlib-1.2.8-2.fc20.noarch.rpm
   echo "USE_SYSTEM_ZLIB = 1" >> Make.user
+  
+  # Move all downloaded lib and bin files into build tree
+  mv usr/x86_64-w64-mingw32/sys-root/mingw/lib/*.dll.a usr/lib
+  mv usr/x86_64-w64-mingw32/sys-root/mingw/bin/* usr/bin
 else
   echo "XC_HOST = i686-pc-mingw32" > Make.user
   echo "override BUILD_MACHINE = i686-pc-cygwin" >> Make.user
   
-  make -C deps get-llvm get-readline get-pcre \
+  make -C deps get-llvm get-openblas get-lapack get-readline get-pcre \
     get-fftw get-gmp get-mpfr get-zlib > get-deps.log 2>&1
   # OpenBlas uses HOSTCC to compile getarch, but we might not have Cygwin GCC installed
   if [ -z `which gcc 2>/dev/null` ]; then
@@ -99,7 +112,7 @@ else
 fi
 
 #make -C deps getall >> get-deps.log 2>&1
-make -C depsget-openblas get-lapack get-uv get-double-conversion get-openlibm get-openspecfun get-random \
+make -C deps get-uv get-double-conversion get-openlibm get-openspecfun get-random \
   get-suitesparse get-arpack get-unwind get-osxunwind get-patchelf get-utf8proc >> get-deps.log 2>&1
 
 if [ -n "`file deps/libuv/missing | grep CRLF`" ]; then
