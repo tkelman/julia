aae08061a09156949853f982266652a71a2740ff
Tony Kelman <tony@kelman.net>
2014-03-31 19:14:41 -0700

    Additions to master for a working MSVC REPL
    assembly fixes, add -lgetopt for linking REPL exe,
    add MSVC port of getopt and an LLVM header fix to msys_build.sh
    
    temporary non-ideal solutions, not suitable for a PR

---
+++ contrib/windows/msys_build.sh
@@ -0,0 +1,203 @@
+#!/bin/sh
+# Script to compile Julia in MSYS assuming 7zip is installed and on the path,
+# or Cygwin assuming make, curl, and mingw64-$ARCH-gcc-g++ are installed
+
+# Run in top-level Julia directory
+cd `dirname "$0"`/../..
+# Stop on error
+set -e
+
+# If ARCH environment variable not set, choose based on uname -m
+if [ -z "$ARCH" ]; then
+  export ARCH=`uname -m`
+fi
+if [ $ARCH = x86_64 ]; then
+  bits=64
+  archsuffix=64
+else
+  bits=32
+  archsuffix=86
+fi
+echo "override ARCH = $ARCH" > Make.user
+
+# Set XC_HOST if in Cygwin
+if [ -n "`uname | grep CYGWIN`" ]; then
+  if [ -z "$XC_HOST" ]; then
+    export XC_HOST="$ARCH-w64-mingw32"
+  fi
+  echo "override BUILD_MACHINE = $ARCH-pc-cygwin" >> Make.user
+  CROSS_COMPILE="$XC_HOST-"
+  # Set HOSTCC if we don't have Cygwin gcc installed
+  if [ -z "`which gcc 2>/dev/null`" ]; then
+    echo 'override HOSTCC = $(CROSS_COMPILE)gcc' >> Make.user
+  fi
+else
+  CROSS_COMPILE=""
+fi
+
+# Download most recent Julia binary for dependencies
+echo "" > get-deps.log
+if [ -z "`which julia-installer.exe 2>/dev/null`" ]; then
+  f=julia-0.3.0-prerelease-win$bits.exe
+  echo "Downloading $f"
+  curl -kLOsS http://s3.amazonaws.com/julialang/bin/winnt/x$archsuffix/0.3/$f
+  echo "Extracting $f"
+  7z x -y $f >> get-deps.log
+fi
+for i in bin/*.dll lib/julia/*.a include/julia/uv*.h include/julia/tree.h \
+    Git/bin/msys-1.0.dll Git/bin/msys-perl5_8.dll Git/bin/perl.exe Git/bin/sh.exe; do
+  7z e -y julia-installer.exe "\$_OUTDIR/$i" \
+    -ousr\\`dirname $i | sed -e 's|/julia||' -e 's|/|\\\\|g'` >> get-deps.log
+done
+# Remove libjulia.dll if it was copied from downloaded binary
+rm -f usr/bin/libjulia.dll
+rm -f usr/bin/libjulia-debug.dll
+
+mingw=http://sourceforge.net/projects/mingw
+if [ -z "$USEMSVC" ]; then
+  if [ -z "`which ${CROSS_COMPILE}gcc 2>/dev/null`" ]; then
+    f=mingw-w$bits-bin-$ARCH-20140102.7z
+    if ! [ -e $f ]; then
+      echo "Downloading $f"
+      curl -kLOsS $mingw-w64-dgn/files/mingw-w64/$f
+    fi
+    echo "Extracting $f"
+    7z x -y $f >> get-deps.log
+    export PATH=$PATH:$PWD/mingw$bits/bin
+    # If there is a version of make.exe here, it is mingw32-make which won't work
+    rm -f mingw$bits/bin/make.exe
+  fi
+  export AR=${CROSS_COMPILE}ar
+
+  f=llvm-3.3-$ARCH-w64-mingw32-juliadeps.7z
+else
+  # compile and ar-lib scripts to use MSVC instead of MinGW compiler
+  curl -kLOsS3 https://raw.githubusercontent.com/Distrotech/automake/v1.14.1/lib/compile
+  curl -kLOsS3 https://raw.githubusercontent.com/Distrotech/automake/v1.14.1/lib/ar-lib
+  # Create a modified version of compile for wrapping link
+  sed -e 's/-link//' -e 's/cl/link/g' -e 's/ -Fe/ -OUT:/' \
+    -e 's|$dir/$lib|$dir/lib$lib|g' compile > linkld
+  chmod +x compile
+  chmod +x ar-lib
+  chmod +x linkld
+  export CC="$PWD/compile cl -nologo"
+  export AR="$PWD/ar-lib lib"
+  export LD="$PWD/linkld link"
+  echo "override CC = $CC" >> Make.user
+  echo 'override CXX = $(CC)' >> Make.user
+  echo "override AR = $AR" >> Make.user
+  echo "override LD = $LD" >> Make.user
+
+  f=llvm-3.3.1-$ARCH-msvc11-juliadeps.7z
+fi
+
+if ! [ -e $f ]; then
+  echo "Downloading $f"
+  curl -kLOsS http://sourceforge.net/projects/juliadeps-win/files/$f
+fi
+echo "Extracting $f"
+7z x -y $f >> get-deps.log
+echo 'LLVM_CONFIG = $(JULIAHOME)/usr/bin/llvm-config' >> Make.user
+# The MinGW binary version of LLVM doesn't include libgtest or libgtest_main
+$AR cr usr/lib/libgtest.a
+$AR cr usr/lib/libgtest_main.a
+
+# If no Fortran compiler installed, override with name of C compiler
+# (this only fixes the unnecessary invocation of FC in openlibm)
+if [ -z "`which ${CROSS_COMPILE}gfortran 2>/dev/null`" ]; then
+  echo 'override FC = $(CC)' >> Make.user
+fi
+
+if [ -z "`which make 2>/dev/null`" ]; then
+  download="/make/make-3.81-2/make-3.81-2-msys-1.0.11-bin.tar"
+  if [ -n "`uname | grep CYGWIN`" ]; then
+    echo "Install the Cygwin package for 'make' and try again."
+    exit 1
+  fi
+else
+  download=""
+fi
+for f in $download \
+    /coreutils/coreutils-5.97-2/coreutils-5.97-2-msys-1.0.11-bin.tar; do
+  if ! [ -e `basename $f.lzma` ]; then
+    echo "Downloading `basename $f`"
+    curl -kLOsS $mingw/files/MSYS/Base$f.lzma
+  fi
+  7z x -y `basename $f.lzma` >> get-deps.log
+  tar -xf `basename $f`
+done
+if [ -z "`which make 2>/dev/null`" ]; then
+  mv bin/make.exe /usr/bin
+  # msysgit has an ancient version of touch that fails with `touch -c nonexistent`
+  cp bin/touch.exe /usr/bin
+fi
+for i in cat chmod echo false printf sort touch true; do
+  mv bin/$i.exe usr/Git/bin
+done
+
+f=mingw$bits-pcre-8.34-1.fc21.noarch
+if ! [ -e $f.rpm ]; then
+  echo "Downloading $f"
+  curl -kLOsS ftp://rpmfind.net/linux/fedora/linux/development/rawhide/x86_64/os/Packages/m/$f.rpm
+fi
+7z x -y $f.rpm >> get-deps.log
+7z x -y $f.cpio >> get-deps.log
+echo 'override PCRE_CONFIG = $(JULIAHOME)/usr/bin/pcre-config' >> Make.user
+# Move downloaded bin, lib, and include files into build tree
+mv usr/$ARCH-w64-mingw32/sys-root/mingw/bin/* usr/bin
+mv usr/$ARCH-w64-mingw32/sys-root/mingw/lib/*.dll.a usr/lib
+mv usr/$ARCH-w64-mingw32/sys-root/mingw/include/* usr/include
+# Modify prefix in pcre-config
+sed -i "s|prefix=/usr/$ARCH-w64-mingw32/sys-root/mingw|prefix=$PWD/usr|" usr/bin/pcre-config
+chmod +x usr/bin/*
+
+for lib in LLVM SUITESPARSE ARPACK BLAS LAPACK FFTW GMP MPFR \
+    PCRE LIBUNWIND GRISU RMATH OPENSPECFUN LIBUV; do
+  echo "USE_SYSTEM_$lib = 1" >> Make.user
+done
+echo 'LIBBLAS = -L$(JULIAHOME)/usr/bin -lopenblas' >> Make.user
+echo 'LIBBLASNAME = libopenblas' >> Make.user
+echo 'override LIBLAPACK = $(LIBBLAS)' >> Make.user
+echo 'override LIBLAPACKNAME = $(LIBBLASNAME)' >> Make.user
+echo 'override LIBUV = $(JULIAHOME)/usr/lib/libuv.a' >> Make.user
+echo 'override LIBUV_INC = $(JULIAHOME)/usr/include' >> Make.user
+
+# Remaining dependencies:
+# openlibm since we need it as a static library to work properly
+# utf8proc since its headers are not in the binary download
+echo 'override STAGE1_DEPS = ' >> Make.user
+echo 'override STAGE2_DEPS = utf8proc' >> Make.user
+echo 'override STAGE3_DEPS = ' >> Make.user
+make -C deps get-openlibm get-utf8proc
+
+# Disable git and enable verbose make in AppVeyor
+if [ -n "$APPVEYOR" ]; then
+  echo 'override NO_GIT = 1' >> Make.user
+  echo 'VERBOSE = 1' >> Make.user
+fi
+
+if [ -n "$USEMSVC" ]; then
+  # Openlibm doesn't build well with MSVC right now
+  echo 'USE_SYSTEM_OPENLIBM = 1' >> Make.user
+  # Since we don't have a static library for openlibm
+  echo 'override UNTRUSTED_SYSTEM_LIBM = 0' >> Make.user
+
+  # Download and compile an implementation of getopt that works with MSVC
+  f=full-getopt-port-for-unicode-and-multibyte-microso
+  rm -rf $f
+  git clone -q https://git.codeproject.com/ludvik-jerabek/$f
+  $CC -Wl,-dll -DEXPORTS_GETOPT -o usr/bin/libgetopt.dll $f/getopt_mb_uni_src/getopt.c
+  cp $f/getopt_mb_uni_src/getopt.h usr/include
+
+  # Fix an LLVM header bug for 64-bit MSVC
+  sed -i 's/typedef signed int ssize_t;/typedef intptr_t ssize_t;/' usr/include/llvm/Support/DataTypes.h
+
+  # Compile libuv and utf8proc without -TP first, then add -TP
+  make -C deps install-uv install-utf8proc
+  cp usr/lib/uv.lib usr/lib/libuv.a
+  echo 'override CC += -TP' >> Make.user
+else
+  echo 'override STAGE1_DEPS += openlibm' >> Make.user
+fi
+
+make -j 4
