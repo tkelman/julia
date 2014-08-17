54e0896a992f7947b23451607dc39726ea487b43
Tony Kelman <tony@kelman.net>
2014-08-17 13:56:13 -0700

    Delete werckerbuild.sh
    [ci skip]

--- contrib/werckerbuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# stop on failure
-set -e
-uname -a
-sudo apt-get install g++-mingw-w64-i686 gfortran-mingw-w64-i686
-echo 'XC_HOST = i686-w64-mingw32' >> Make.user
-make win-extras
-make dist
