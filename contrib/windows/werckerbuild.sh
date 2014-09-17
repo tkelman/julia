35f1549552f61c0f444f9af587ce1206f0538be1
Tony Kelman <tony@kelman.net>
2014-09-16 21:47:06 -0700

    compress dependency tarballs

--- contrib/windows/werckerbuild.sh
+++ contrib/windows/werckerbuild.sh
@@ -19,32 +19,33 @@ for XC_HOST in i686-w64-mingw32 x86_64-w64-mingw32; do
   make distcleanall > /dev/null 2>&1
   rm -rf dist-extras
   mkdir -p $WERCKER_CACHE_DIR/$XC_HOST
-  if ! [ -e $WERCKER_CACHE_DIR/$XC_HOST/dist-extras.tar ]; then
+  if ! [ -e $WERCKER_CACHE_DIR/$XC_HOST/dist-extras.tar.gz ]; then
     make win-extras
-    tar -cf $WERCKER_CACHE_DIR/$XC_HOST/dist-extras.tar dist-extras
+    tar -czf $WERCKER_CACHE_DIR/$XC_HOST/dist-extras.tar.gz dist-extras
   fi
   mkdir -p usr/bin
-  if [ -e $WERCKER_CACHE_DIR/$XC_HOST/usr.tar ]; then
-    tar -xf $WERCKER_CACHE_DIR/$XC_HOST/usr.tar
+  if [ -e $WERCKER_CACHE_DIR/$XC_HOST/usr.tar.gz ]; then
+    tar -xzf $WERCKER_CACHE_DIR/$XC_HOST/usr.tar.gz
   fi
-  for depgroup in "llvm fftw gmp mpfr pcre dsfmt" "openblas suitesparse arpack"; do
+  for depgroup in "llvm fftw gmp mpfr pcre" "openblas suitesparse-wrapper arpack"; do
     if [ $cache_ready = 1 ]; then
       for i in $depgroup; do
-        if [ -e $WERCKER_CACHE_DIR/$XC_HOST/$i.tar ]; then
-          tar -xf $WERCKER_CACHE_DIR/$XC_HOST/$i.tar
+        if [ -e $WERCKER_CACHE_DIR/$XC_HOST/$i.tar.gz ]; then
+          tar -xzf $WERCKER_CACHE_DIR/$XC_HOST/$i.tar.gz
         else
           cache_ready=0
         fi
         make -j8 -C deps install-$i
-        if [ $i = suitesparse ]; then
-          tar -cf $WERCKER_CACHE_DIR/$XC_HOST/$i.tar deps/SuiteSparse-*/ deps/SuiteSparse-*.tar.gz
+        if [ $i = suitesparse-wrapper ]; then
+          tar -czf $WERCKER_CACHE_DIR/$XC_HOST/$i.tar.gz deps/SuiteSparse-*/ deps/SuiteSparse-*.tar.gz
         else
-          tar -cf $WERCKER_CACHE_DIR/$XC_HOST/$i.tar deps/$i-*/ deps/$i-*.tar.*
+          tar -czf $WERCKER_CACHE_DIR/$XC_HOST/$i.tar.gz deps/$i-*/ deps/$i-*.tar.*
         fi
       done
     fi
   done
-  tar -cf $WERCKER_CACHE_DIR/$XC_HOST/usr.tar usr
+  tar -czf $WERCKER_CACHE_DIR/$XC_HOST/usr.tar.gz usr
+  unset XC_HOST
 done
 
 # do make dist, if all the dependencies are ready for both 32 and 64 bit
@@ -53,13 +54,14 @@ if [ $cache_ready = 1 ]; then
     export XC_HOST=$ARCH-w64-mingw32
     make distcleanall > /dev/null 2>&1
     rm -rf dist-extras
-    tar -xf $WERCKER_CACHE_DIR/$XC_HOST/dist-extras.tar
-    tar -xf $WERCKER_CACHE_DIR/$XC_HOST/usr.tar
+    tar -xzf $WERCKER_CACHE_DIR/$XC_HOST/dist-extras.tar.gz
+    tar -xzf $WERCKER_CACHE_DIR/$XC_HOST/usr.tar.gz
     for i in llvm fftw gmp mpfr pcre dsfmt openblas suitesparse arpack; do
-      tar -xf $WERCKER_CACHE_DIR/$XC_HOST/$i.tar
+      tar -xzf $WERCKER_CACHE_DIR/$XC_HOST/$i.tar.gz
     done
     make -j8 dist
     curl -T julia-*.exe -utkelman:$BINTRAYKEY "https://api.bintray.com/content/tkelman/generic/Julia/0.4.0-dev/julia-0.4.0-dev-$WERCKER_GIT_COMMIT-$ARCH.exe;publish=1"
     mv julia-*.exe $WERCKER_OUTPUT_DIR
+    unset XC_HOST
   done
 fi
