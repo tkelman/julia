ca3467f39ff692fc19672df31b6b8a023cdc1495
Tony Kelman <tony@kelman.net>
2014-11-20 20:03:25 -0800

    save primary.xml to disk instead of string variable

--- contrib/windows/winrpm.sh
+++ contrib/windows/winrpm.sh
@@ -3,6 +3,10 @@
 # ./winrpm.sh http://download.opensuse.org/repositories/windows:/mingw:/win64/openSUSE_13.1/ mingw64-hdf5
 # depends on curl, xmllint, gunzip
 
+#PS4='+ $(date "+%s.%N")\011 '
+#exec 3>&2 2>/tmp/bashstart.$$.log
+#set -x
+
 set -e
 url=$1
 toinstall=$2
@@ -23,19 +27,21 @@ loc="local-name()="
 eval $(retry_curl $url/repodata/repomd.xml | $xp "/*[$loc'repomd'] \
   /*[$loc'data'][@type='primary']/*[$loc'location']/@href" -)
 
+mkdir -p repodata
 case $href in
   *.gz)
-    primary=$(retry_curl $url/$href | gunzip);;
+    retry_curl $url/$href | gunzip > $href;;
   *)
-    primary=$(retry_curl $url/$href);;
+    retry_curl -o $href $url/$href;;
 esac
+#href=primary.xml
 
 # outputs <package> xml string for newest version
 # don't include arch=src packages, those will list build-time dependencies
 rpm_select() {
-  candidates="<c>$(echo $primary | $xp "//*[$loc'package'] \
-    [./*[$loc'name' and .='$1']][./*[$loc'arch' and .='noarch']]" - \
-    2>/dev/null | sed -e 's|<rpm:|<|g' -e 's|</rpm:|</|g')</c>"
+  candidates="<c>$($xp "//*[$loc'package'][./*[$loc'name' and .='$1']] \
+    [./*[$loc'arch' and .='noarch']]" $href 2>/dev/null | \
+    sed -e 's|<rpm:|<|g' -e 's|</rpm:|</|g')</c>"
   # remove rpm namespacing so output can be parsed by xmllint later
   if [ "$candidates" = "<c></c>" ]; then
     echo "error: no package candidates found for $1" >&2
@@ -78,8 +84,8 @@ done
 
 # outputs package and dll names, e.g. mingw64(zlib1.dll)
 rpm_requires() {
-  for i in $(rpm_select $1 | $xp "/package/format/requires/entry/@name" - \
-      2>/dev/null); do
+  for i in $(rpm_select $1 | \
+      $xp "/package/format/requires/entry/@name" - 2>/dev/null); do
     eval $i
     echo $name
   done
@@ -91,9 +97,9 @@ rpm_requires() {
 
 # outputs package name, fails if multiple providers with different names
 rpm_provides() {
-  providers=$(echo $primary | $xp "//*[$loc'package'] \
-    [./*[$loc'format']/*[$loc'provides']/*[$loc'entry'][@name='$1']] \
-    /*[$loc'name']" - | sed -e 's|<name>||g' -e 's|</name>|\n|g' | sort -u)
+  providers=$($xp "//*[$loc'package'][./*[$loc'format']/*[$loc'provides'] \
+    /*[$loc'entry'][@name='$1']]/*[$loc'name']" $href | \
+    sed -e 's|<name>||g' -e 's|</name>|\n|g' | sort -u)
   if [ $(echo $providers | wc -w) -gt 1 ]; then
     echo "found multiple providers $providers for $1" >&2
     echo "can't decide which to pick, bailing" >&2
@@ -115,8 +121,9 @@ while [ -n "$newpkgs" ]; do
     requires="$(rpm_requires $i)"
     #echo "requires of $i:"
     #echo $requires
-    case "$allrequires $newrequires" in
-      *$requires*) # already on list
+    # leading and trailing spaces to ensure word match
+    case " $allrequires $newrequires " in
+      *" $requires "*) # already on list
         ;;
       *)
         newrequires="$newrequires $requires";;
@@ -130,8 +137,8 @@ while [ -n "$newpkgs" ]; do
     provides="$(rpm_provides $i)"
     #echo "provides $i:"
     #echo $provides
-    case "$toinstall $newpkgs" in
-      *$provides*) # already on list
+    case " $toinstall $newpkgs " in
+      *" $provides "*) # already on list
         ;;
       *)
         newpkgs="$newpkgs $provides";;
@@ -142,3 +149,8 @@ while [ -n "$newpkgs" ]; do
   toinstall="$toinstall $newpkgs"
 done
 echo "packages to install: $toinstall"
+
+#set +x
+#exec 2>&3 3>&-
+
+#rm $href
