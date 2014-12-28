e49fc0d1d5b3a8816f36a39266456e8ea6c10ed5
Tony Kelman <tony@kelman.net>
2014-12-28 00:00:04 -0800

    add an rpm namespace, other little tweaks

--- contrib/windows/winrpm.ps1
+++ contrib/windows/winrpm.ps1
@@ -5,13 +5,13 @@ $url = 'http://download.opensuse.org/repositories/windows:mingw:/win64/openSUSE_
 [Xml]$repomd = (New-Object Net.WebClient).DownloadString($url + 'repodata/repomd.xml')
 $ns = New-Object Xml.XmlNamespaceManager($repomd.NameTable)
 $ns.AddNamespace('ns', $repomd.DocumentElement.NamespaceURI)
-$primarygz = (New-Object Net.WebClient).DownloadData($url + $repomd.SelectSingleNode(
+$primarygz = (New-Object Net.WebClient).DownloadData($url + $repomd.SelectSingleNode( `
     "/ns:repomd/ns:data[@type='primary']/ns:location/@href", $ns).'#text')
 
 # I don't understand how `curl | gunzip` can possibly require this many lines of code?!?
 $instream = New-Object IO.MemoryStream
 $outstream = New-Object IO.MemoryStream
-$gz = New-Object IO.Compression.GzipStream($instream, ([IO.Compression.CompressionMode]::Decompress))
+$gz = New-Object IO.Compression.GZipStream($instream, ([IO.Compression.CompressionMode]::Decompress))
 $instream.Write($primarygz, 0, $primarygz.Length)
 $instream.Position = 0
 try {
@@ -33,5 +33,18 @@ finally {
 
 $ns = New-Object Xml.XmlNamespaceManager($primary.NameTable)
 $ns.AddNamespace('ns', $primary.DocumentElement.NamespaceURI)
+$ns.AddNamespace('rpm', $primary.DocumentElement.rpm)
 
 $primary.SelectNodes("//ns:package[ns:name='mingw64-hdf5' and ns:arch='noarch']", $ns)
+
+
+
+
+
+[Xml]$prim2 = Get-Content 'D:\cygwin64\home\Tony\julia\contrib\windows\primary.xml'
+$prim2.SelectNodes("//ns:package[ns:name='mingw64-libgit2' and ns:arch='noarch']/ `
+    ns:format/rpm:requires", $ns)
+
+$prim2.SelectNodes("//ns:package[ns:name='mingw64-hdf5' and ns:arch='noarch']", $ns)[0].version
+$prim2.SelectNodes("//ns:package[ns:name='mingw64-hdf5' and ns:arch='noarch']", $ns)[1].version
+
