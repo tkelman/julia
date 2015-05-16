0db1301e00131a61f633e96b9110d16bef390437
Jameson Nash <vtjnash@gmail.com>
2015-05-15 23:48:28 -0400

    move inference out of Base into Core.Inference
    having inference inside Base made it hard to modify and
    slow to compile the sysimg since bootstrapping.
    additionally, it made inference vulnerable to unintentional
    sabotage by the users program.
    
    to combat those issues, this creates a copy of the essential code
    required for running the inference into Core.Inference.
    this allows any part of the system (esp. Base) around inference to
    change, and even be recompiled or monkey-patched,
    without impacting the core inference algorithms.
    
    note that this clarifies and even tightens the restrictions on what
    functionality is available for the use of inference.
    the primary available functionality is:
      "essential" primitives
      integers
      arrays
      dictionary support (limited, and ObjectIdDict will NOT deserialize properly)
      c-interaction
      intset
      limited printing and string support (no interpolation)

--- base/operators.jl
+++ base/operators.jl
@@ -425,12 +425,14 @@ reverse(p::Pair) = Pair(p.second, p.first)
 # some operators not defined yet
 global //, >:, <|, hcat, hvcat, ⋅, ×, ∈, ∉, ∋, ∌, ⊆, ⊈, ⊊, ∩, ∪, √, ∛
 
-module Operators
+this_module = current_module()
+baremodule Operators
 
 export
     !,
     !=,
     !==,
+    ===,
     $,
     %,
     .%,
@@ -500,9 +502,9 @@ export
     ctranspose,
     call
 
-import Base: !, !=, $, %, .%, &, *, +, -, .!=, .+, .-, .*, ./, .<, .<=, .==, .>,
+import ..this_module: !, !=, $, %, .%, &, *, +, -, .!=, .+, .-, .*, ./, .<, .<=, .==, .>,
     .>=, .\, .^, /, //, <, <:, <<, <=, ==, >, >=, >>, .>>, .<<, >>>,
-    <|, |>, \, ^, |, ~, !==, >:, colon, hcat, vcat, hvcat, getindex, setindex!,
+    <|, |>, \, ^, |, ~, !==, ===, >:, colon, hcat, vcat, hvcat, getindex, setindex!,
     transpose, ctranspose, call,
     ≥, ≤, ≠, .≥, .≤, .≠, ÷, ⋅, ×, ∈, ∉, ∋, ∌, ⊆, ⊈, ⊊, ∩, ∪, √, ∛
 
