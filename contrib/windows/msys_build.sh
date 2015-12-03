305c4ebfb67e92a889f49c6afb31c02aa1f28ca3
Tony Kelman <tony@kelman.net>
2015-12-03 15:52:58 -0500

    test this commit on appveyor

--- contrib/windows/msys_build.sh
+++ contrib/windows/msys_build.sh
@@ -85,9 +85,9 @@ esac
 
 # Download most recent Julia binary for dependencies
 if ! [ -e julia-installer.exe ]; then
-  f=julia-0.4-latest-win$bits.exe
+  f=julia-0.4.2-9f8ec24adb-win$bits.exe
   echo "Downloading $f"
-  $curlflags -O https://s3.amazonaws.com/julialang/bin/winnt/x$archsuffix/0.4/$f
+  $curlflags -O https://s3.amazonaws.com/julianightlies/bin/winnt/x$archsuffix/0.4/$f
   echo "Extracting $f"
   $SEVENZIP x -y $f >> get-deps.log
 fi
