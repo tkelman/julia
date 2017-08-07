d4f14e6eec09ff5544a449c4d043e963f343c01b
Tony Kelman <tony@kelman.net>
2017-08-06 22:39:46 -0700

    compactify _clipboardcmds dict from 12 lines to 6

--- base/interactiveutil.jl
+++ base/interactiveutil.jl
@@ -136,18 +136,12 @@ if Sys.isapple()
 
 elseif Sys.islinux() || Sys.KERNEL === :FreeBSD
     _clipboardcmd = nothing
-    const _clipboardcmds = Dict(
-        :copy => Dict(
-            :xsel  => Sys.islinux() ?
-                `xsel --nodetach --input --clipboard` : `xsel -c`,
-            :xclip => `xclip -silent -in -selection clipboard`,
-        ),
-        :paste => Dict(
-            :xsel  => Sys.islinux() ?
-                `xsel --nodetach --output --clipboard` : `xsel -p`,
-            :xclip => `xclip -quiet -out -selection clipboard`,
-        )
-    )
+    const _clipboardcmds = Dict(:copy => Dict(:xsel => Sys.islinux() ?
+            `xsel --nodetach --input --clipboard` : `xsel -c`,
+            :xclip => `xclip -silent -in -selection clipboard`),
+        :paste => Dict(:xsel => Sys.islinux() ?
+            `xsel --nodetach --output --clipboard` : `xsel -p`,
+            :xclip => `xclip -quiet -out -selection clipboard`))
     function clipboardcmd()
         global _clipboardcmd
         _clipboardcmd !== nothing && return _clipboardcmd
